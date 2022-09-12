ff2f5f12539140940e0e1f9b0be2b6be21f89355
mattre1d <matt.reid@outlook.com.au>
2022-09-12 22:10:48 +1000

    Initial Commit

---
+++ VIS Service Tool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VIS Service Tool
+//
+//  Created by Matt Reid on 12/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VIS Service Tool/VIS_Service_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIS_Service_ToolApp.swift
+//  VIS Service Tool
+//
+//  Created by Matt Reid on 12/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct VIS_Service_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
